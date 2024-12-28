18b376e6b700ab3142700270d956e2367651de7e
Stijn Veenman <veenman.stijn@gmail.com>
2024-12-28 17:39:06 +0100

    remove some description stuff from main flake.nix

--- flake.nix
+++ flake.nix
@@ -6,8 +6,6 @@
       name,
       description,
       path,
-      buildTools ? null,
-      additionalSetupInfo ? null,
     }: {
       inherit path;
 
@@ -17,41 +15,12 @@
         # ${name}
         ${description}
 
-        ${
-          if buildTools != null
-          then ''
-            Comes bundled with:
-            ${builtins.concatStringsSep ", " buildTools}
-          ''
-          else ""
-        }
-        ${
-          if additionalSetupInfo != null
-          then ''
-            ## Additional Setup
-            To set up the project run:
-            ```sh
-            flutter create .
-            ```
-          ''
-          else ""
-        }
         ## Other tips
-        If you use direnv run:
+        To load the new devShell run:
 
         ```
-            echo "use flake" > .envrc
+            direnv allow
         ```
-
-        For a quick license setup use licensor:
-
-        ```
-            # SPDX is the license id like MIT or GPL-3.0
-            nix-shell -p licensor --command "licensor SPDX > LICENSE"
-        ```
-
-        ## More info
-        - [flake-utils Github Page](https://github.com/numtide/flake-utils)
       '';
     };
   in {
